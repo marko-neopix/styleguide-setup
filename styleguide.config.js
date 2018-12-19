26477106ba0840bf8c1d8017741dab3e82ee015e
Marko Ilic <marko@weareneopix.com>
2018-12-19 14:41:33 +0100

    Styleguide init

---
+++ styleguide.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // set your styleguidist configuration here
+  title: 'Default Style Guide',
+  defaultExample: true,
+  styleguideDir: 'docs'
+  // components: 'src/components/**/[A-Z]*.vue',
+  // sections: [
+  //   {
+  //     name: 'First Section',
+  //     components: 'src/components/**/[A-Z]*.vue'
+  //   }
+  // ],
+  // webpackConfig: {
+  //   // custom config goes here
+  // }
+}
